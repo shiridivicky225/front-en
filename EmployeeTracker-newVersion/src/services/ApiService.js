9ee8312fd6f84b694f7a2597c09324b270d649d6
shiridivicky225 <103015998+shiridivicky225@users.noreply.github.com>
2022-08-09 10:04:12 +0530

    Update ApiService.js

--- EmployeeTracker-newVersion/src/services/ApiService.js
+++ EmployeeTracker-newVersion/src/services/ApiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const BASE_URL = 'https://lsi-employeetracker.herokuapp.com'; // heruko.
-const BASE_URL = "http://34.239.103.102:8080/LESM-Status-Monitor-0.0.1-SNAPSHOT"; // devops
+const BASE_URL = "http://44.204.79.161:8080/LESM-Status-Monitor-0.0.1-SNAPSHOT"; // devops
 // const BASE_URL = "http://10.81.4.195:2022"; // umer pc
 // const BASE_URL = "http://10.81.3.30:9090"; // charan pc
 //Header
